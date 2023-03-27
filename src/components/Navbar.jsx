3168f881db1256191ed1661feed771cc48ebf5c3
shastin245 <valexanderjustin@gmail.com>
2023-03-27 15:11:15 -0600

    Creación del navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+export default function Navbar(){
+    return <nav className="nav">
+    <a href=" Rick & Morty" className="rymsite">
+    </a>
+    <ul>
+        <li>
+            <a href="/home">Home</a>
+            <a href="/char">Personajes</a>
+            <a href="/about">Nosotros</a>
+            <a href="/login">Iniciar sesión</a>
+        </li>
+    </ul>
+    </nav>
+}
