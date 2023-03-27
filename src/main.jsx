4e7e95aa7f249c9abd85ea84e25f535f821553a1
Fatima Monterola <125491660+fatimamonterola@users.noreply.github.com>
2023-03-27 16:38:45 -0600

    modifique archivo main

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import {Auth0Provider} from "@auth0/auth0-react"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+console.log(
+  process.env.REACT_APP_AUTH0_DOMAIN,
+  process.env.REACT_APP_AUTH0_CLIENT_ID
+)
+
+ReactDOM.render(
   <React.StrictMode>
+    <Auth0Provider>
+     
     <App />
+    </Auth0Provider>
+    
   </React.StrictMode>,
+  document.getElementById("root")
 )
