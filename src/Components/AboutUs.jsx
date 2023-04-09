d83483352dc476e3f6f9c10b230964f293214d7c
jonfm09 <jonathan.fermanm@gmail.com>
2023-04-08 19:08:46 -0600

    Update 3 About Us

--- src/Components/AboutUs.jsx
+++ src/Components/AboutUs.jsx
@@ -9,63 +9,63 @@ export default function AboutUs(props) {
       
             <div class="">
               
-              <div class = "row align-items-left"> 
-              
-                <div class="col-md-6 align-middle container p-4">
+                <div class = "row align-items-left"> 
+                
+                  <div class="col-md-6 align-middle container p-4">
 
-                  <img src="Resource/Justin.png"  alt="" />
+                    <img src="Resource/Justin.png"  alt="" />
 
+                  </div>
+                  <div class="col-md-6 container p-4  ">
+
+                    <h5 class="text-aling-right fw-light"> Justin Romero </h5> 
+                    <h1> FRONTEND </h1>  
+                    
+                  </div>
+                
+                
                 </div>
-                <div class="col-md-6 container p-4  ">
-
-                  <h5 class="text-aling-right fw-light"> Justin Romero </h5> 
-                  <h1> FRONTEND </h1>  
-                  
-                </div>
-              
-              
-              </div>
                 
            </div>
       
 
            <div class="">
               
-              <div class = "row"> 
-              
-                <div class="col-md-6 container p-4  text-end ">
-                  <h5 class="text-aling-right fw-light"> Rocio Chicas </h5> 
-                  <h1 class="text-aling-right"> FULL STACK </h1> 
+                <div class = "row"> 
+                
+                  <div class="col-md-6 text-end">
+                    <h5 class="text-aling-right fw-light"> Rocio Chicas </h5> 
+                    <h1 class="text-aling-right"> FULL STACK </h1> 
 
+                  </div>
+
+                  <div class="col-md-6">
+
+                    <img src="Resource/Rocio.png"  alt="" /> 
+    
+                  </div>
+                
+                
                 </div>
-
-                <div class="col-md-6 container p-4">
-
-                  <img src="Resource/Rocio.png"  alt="" /> 
-  
-                </div>
-              
-              
-              </div>
                 
            </div>
       
 
            <div class="">
               
-              <div class = "row align-items-left"> 
-              
-                <div class="col-md-6 container p-4">
+                <div class = "row align-items-left"> 
+                
+                  <div class="col-md-6 ">
 
-                  <img src="Resource/Fatima.png"  alt="" />
+                    <img src="Resource/Fatima.png"  alt="" />
 
-                </div>
-                <div class="col-md-6 container p-4 ">
-                  <h5 class="text-aling-right fw-light"> Fatima Monterola </h5> 
-                  <h1 class=""> BACKEND </h1>  
-                  
-                </div>
-              
+                  </div>
+                  <div class="col-md-6 ">
+                    <h5 class="text-aling-right fw-light"> Fatima Monterola </h5> 
+                    <h1 class=""> BACKEND </h1>  
+                    
+                  </div>
+                
               
               </div>
                 
@@ -74,29 +74,27 @@ export default function AboutUs(props) {
 
            <div class="">
               
-              <div class = "row"> 
-              
-                <div class="col-md-6 container p-4 text-end">
+                <div class = "row"> 
+                
+                  <div class="col-md-6 text-end ">
 
-                 
-                <h5 class="text-aling-right fw-light"> Jonathan Ferman </h5> 
-                  <h1> FRONTEND </h1>
                   
+                  <h5 class="text-aling-right fw-light aling-item-center"> Jonathan Ferman </h5> 
+                    <h1> FRONTEND </h1>
+                    
 
+                  </div>
+
+                  <div class="col-md-6">
+
+                    <img src="Resource/JF.png"  alt="" /> 
+    
+                  </div>
+                
+                
                 </div>
-
-                <div class="col-md-6 container p-4 ">
-
-                  <img src="Resource/JF.png"  alt="" /> 
-  
-                </div>
-              
-              
-              </div>
                 
            </div>
-
-
 
 
 
