f167a48dd57258cc19f18972802b4aa21c33281f
jonfm09 <jonathan.fermanm@gmail.com>
2023-04-08 18:31:27 -0600

    AboutUs Update

---
+++ src/Components/AboutUs.jsx
@@ -0,0 +1,106 @@
+import React from 'react'
+
+export default function AboutUs(props) {
+  return (
+    
+    <div>
+      
+      
+      
+            <div class="">
+              
+              <div class = "row align-items-left"> 
+              
+                <div class="col-md-6 container p-4">
+
+                  <img src="Resource/Justin.png"  alt="" />
+
+                </div>
+                <div class="col-md-6 container p-4 ">
+
+                  <h1 class=""> FRONTEND </h1>  
+                  
+                </div>
+              
+              
+              </div>
+                
+           </div>
+      
+
+           <div class="">
+              
+              <div class = "row"> 
+              
+                <div class="col-md-6 container p-4">
+
+                  <h1 class="text-aling-right"> FULL STACK </h1> 
+
+                </div>
+
+                <div class="col-md-6 container p-4 ">
+
+                  <img src="Resource/Rocio.png"  alt="" /> 
+  
+                </div>
+              
+              
+              </div>
+                
+           </div>
+      
+
+           <div class="">
+              
+              <div class = "row align-items-left"> 
+              
+                <div class="col-md-6 container p-4">
+
+                  <img src="Resource/Fatima.png"  alt="" />
+
+                </div>
+                <div class="col-md-6 container p-4 ">
+
+                  <h1 class=""> BACKEND </h1>  
+                  
+                </div>
+              
+              
+              </div>
+                
+           </div>
+
+
+           <div class="">
+              
+              <div class = "row"> 
+              
+                <div class="col-md-6 container p-4">
+
+                  <h1 class="text-aling-right"> FRONTEND </h1> 
+
+                </div>
+
+                <div class="col-md-6 container p-4 ">
+
+                  <img src="Resource/JF.png"  alt="" /> 
+  
+                </div>
+              
+              
+              </div>
+                
+           </div>
+
+
+
+
+
+     </div>
+  )
+}
+
+
+
+
+
