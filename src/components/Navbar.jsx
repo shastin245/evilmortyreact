70aaa37b491a60028b6a72f06f48379e1e2b9ad1
shastin245 <valexanderjustin@gmail.com>
2023-04-11 19:29:15 -0600

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter, Route, Routes, Link} from 'react-router-dom'
 import List from './List'
 import List2 from './List2'
 import List3 from './List3'
-
+import Appj from './Appj'
 import Search from './Search'
 import Search2 from './Search2'
 import './Navbar.css'
@@ -56,7 +56,7 @@ export default function Navbar(props) {
                 </div>
             </nav>
             <Routes>
-                <Route path='/' element={<List/>}/>
+                <Route path='/' element={<Appj/>}/>
                 <Route path='/Lista1' element={<List/>}/>
                 <Route path='/Lista2' element={<List2/>}/>                   
                 <Route path='/Lista3' element={<List3/>}/>              
@@ -64,7 +64,7 @@ export default function Navbar(props) {
                 <Route path='/Sofia' element={<List/>}/>
                 <Route path='/Nuria' element={<List/>}/>
                 <Route path='/Rocio' element={<List/>}/>
-                <Route path='/Justin' element={<Home/>}/>
+                <Route path='/Justin' element={<List/>}/>
                 <Route path='/Jonathan' element={<List/>}/>                
                 <Route path='/Buscador' element={<Search/>}/>
             </Routes>
