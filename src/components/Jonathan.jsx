d908fe1662699dec0ce6e5a91320a139e2b51ebe
shastin245 <valexanderjustin@gmail.com>
2023-04-11 19:04:31 -0600

    Unión del proyecto
    Casi casi

---
+++ src/components/Jonathan.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import AboutUs from './Components/AboutUs'
+
+
+function App() {
+ 
+
+  return (
+    <div>
+      <AboutUs/>
+    </div>
+  )
+}
+
+export default Jonathan
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes, Link} from 'react-router-dom'
+import Home from './Home'
 import List from './List'
 import List2 from './List2'
 import List3 from './List3'
+import Jonathan from './Jonathan'
 import Search from './Search'
 import Search2 from './Search2'
 import './Navbar.css'
@@ -54,7 +56,7 @@ export default function Navbar(props) {
                 </div>
             </nav>
             <Routes>
-                <Route path='/' element={<List/>}/>
+                <Route path='/' element={<Home/>}/>
                 <Route path='/Lista1' element={<List/>}/>
                 <Route path='/Lista2' element={<List2/>}/>                   
                 <Route path='/Lista3' element={<List3/>}/>              
@@ -63,7 +65,7 @@ export default function Navbar(props) {
                 <Route path='/Nuria' element={<List/>}/>
                 <Route path='/Rocio' element={<List/>}/>
                 <Route path='/Justin' element={<List/>}/>
-                <Route path='/Johnatan' element={<List/>}/>                
+                <Route path='/Jonathan' element={<Jonathan/>}/>                
                 <Route path='/Buscador' element={<Search/>}/>
             </Routes>
         </BrowserRouter>
